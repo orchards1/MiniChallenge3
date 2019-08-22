27a200ba592f79c8a65a8dfd715df06e7af880de
Michael <jagosikat@yahoo.co.id>
2019-08-22 17:42:27 +0700

    Kerangka Onboarding sudah, sisa isi content

--- TestML/ViewController.swift
+++ TestML/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("sadasd")
     }
 
 
